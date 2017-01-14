8fa77d2a9803a64fe143502c079d4e507e20bf02
Pablo Cabezas <pabcabsal@gmail.com>
2017-01-14 15:44:51 +0100

    CircleCI added

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+
+set -e     # Stop on first error
+set -u     # Stop if an unbound variable is referenced
+
+curl -X POST https://api.digitalocean.com/v2/droplets \
+-H "Authorization: Bearer a963fd011e73b202b7ba125c4740796f1a06a845f8c1ce77b06c1de8c4634787"
+
+runcmd:
+    - cd ghosty-backend/
+    - git pull
+    - sh redocker.sh
