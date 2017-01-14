e4a23524eef03974765b2fd1db33ae07c8ad160d
Pablo Cabezas <pabcabsal@gmail.com>
2017-01-14 23:22:52 +0100

    Actualización docker-compose

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,7 @@ set -u     # Stop if an unbound variable is referenced
 curl -X POST https://api.digitalocean.com/v2/droplets \
 -H "Authorization: Bearer a963fd011e73b202b7ba125c4740796f1a06a845f8c1ce77b06c1de8c4634787"
 
-cd ghosty-backend/
-git pull
-sh redocker.sh
+runcmd:
+    - cd ghosty-backend/
+    - git pull
+    - sh redocker.sh
