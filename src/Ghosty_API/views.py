4c72a3abb4410a9c39c32cdb6ef1298b02ec3ee3
Pablo Cabezas <pabcabsal@gmail.com>
2017-02-11 11:18:29 +0100

    Trabajando las vistas de usuarios

--- src/Ghosty_API/views.py
+++ src/Ghosty_API/views.py
@@ -5,7 +5,7 @@ from oauth2_provider.ext.rest_framework import TokenHasReadWriteScope
 from rest_framework import permissions
 from rest_framework import viewsets
 from rest_framework.decorators import parser_classes
-from rest_framework.mixins import CreateModelMixin, UpdateModelMixin
+from rest_framework.mixins import CreateModelMixin, UpdateModelMixin, ListModelMixin, RetrieveModelMixin
 from rest_framework.parsers import JSONParser, FormParser, MultiPartParser
 from rest_framework.renderers import JSONRenderer
 
@@ -13,12 +13,29 @@ from .serializers import UserSerializer, WorkSerializer, DeceasedSerializer, Tas
 
 
 @parser_classes((JSONParser, FormParser, MultiPartParser))
-class UserViewSet(CreateModelMixin, UpdateModelMixin,
+class UserViewSet(CreateModelMixin, UpdateModelMixin, ListModelMixin, RetrieveModelMixin,
                   viewsets.GenericViewSet):
+    permission_classes_by_action = {'default': [permissions.IsAuthenticated()],
+                                    'update': [TokenHasReadWriteScope()],
+                                    'create': [permissions.AllowAny()]}
+
+    def get_permissions(self):
+        try:
+            # return permission_classes depending on `action`
+            return (permission for permission in self.permission_classes_by_action[self.action])
+        except KeyError:
+            # action is not set return default permission_classes
+            return (permission for permission in self.permission_classes_by_action['default'])
+
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return User.objects.all()
+        else:
+            return User.objects.filter(id=self.request.user.id)
+
     queryset = User.objects.all()
     serializer_class = UserSerializer
     renderer_classes = (JSONRenderer,)
-    permission_classes = (permissions.AllowAny,)
 
 
 @parser_classes((JSONParser, FormParser, MultiPartParser))
