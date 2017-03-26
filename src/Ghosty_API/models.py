f9a68b32ca7144b17dadd6c6eb4bcee9a91951ce
Pablo Cabezas <pabcabsal@gmail.com>
2017-03-26 19:20:07 +0200

    Changed time values for now

--- src/Ghosty_API/models.py
+++ src/Ghosty_API/models.py
@@ -1,5 +1,6 @@
 from os.path import basename
 
+import datetime
 import geocoder
 import reversion
 from django.contrib.auth.models import User
@@ -374,7 +375,7 @@ class Work(models.Model):
         (FINISHED, 'Terminado'),
         (CLOSED, 'Cerrado'),
     )
-    a24h = models.CharField(verbose_name="A24H", max_length=50, unique=True, default=timezone.now())
+    a24h = models.CharField(verbose_name="A24H", max_length=50, unique=True, default=datetime.datatime.now())
     employed = models.ForeignKey(User, verbose_name="Empleado", blank=True, null=True)
     deceased = models.OneToOneField(Deceased, verbose_name="Fallecido", blank=True, null=True)
     move = models.OneToOneField(Move, verbose_name="Traslados", blank=True, null=True)
@@ -396,7 +397,7 @@ class Work(models.Model):
     declarant = models.ForeignKey(Declarant, verbose_name="Contratante declarante", blank=True, null=True)
 
     status = models.CharField(verbose_name="Estado", max_length=10, choices=STATES, default=OPEN)
-    creation_date = models.DateField(verbose_name="Fecha de creación", default=timezone.now)
+    creation_date = models.DateField(verbose_name="Fecha de creación", default=datetime.datatime.now())
     finish_date = models.DateField(verbose_name="Fecha de finalización", blank=True, null=True)
     comment = models.TextField(verbose_name="Observaciones", blank=True, null=True)
 
@@ -411,7 +412,7 @@ class Work(models.Model):
 
 def complete_task(sender, instance, **kwargs):
     if instance.status is Task.DONE:
-        instance.done_date = timezone.now()
+        instance.done_date = datetime.datatime.now()
         instance.save()
 
 
